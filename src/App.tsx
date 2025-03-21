b6c546c1b3d2bf3e93c6bb61126d3d57bad81bb0
Astrid Nielsen Labs <32886080+AstridNielsen-lab@users.noreply.github.com>
2025-03-21 05:20:24 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ function App() {
               </a>
             </p>
             <p className="text-sm mt-2">
-             Programador Julio Campos Machado  {' '} </p>
+             Programador Julio Campos Machado  {' '} 
               <a 
                 href="https://character.ai/chat/MpcnLX8s7miWD3XSUa4I6WS4YHRKIL-qJ4AlbADDGrs" 
                 target="_blank" 
@@ -54,7 +54,7 @@ function App() {
               >
               Converse com a Persona
               </a>
-            
+            </p>
           </div>
         </footer>
       </div>
